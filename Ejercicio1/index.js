23e0b6326f2ea696855f57125fa0799dc9a9afe3
angelagumuzzio <angela.gumuzzio@gmail.com>
2021-01-24 21:15:13 +0100

    Ejercicio1

--- Ejercicio1/index.js
+++ Ejercicio1/index.js
@@ -8,3 +8,14 @@ const arealateral=23*13;
 const areabase= 16*23;
 const areaTotal=arealateral+2*areabase;
 console.log('areaTotal', areaTotal+' cm2');
+
+//Generalización
+
+const base=23
+const altura=13
+const ancho=16
+
+const volumen2=base*altura*ancho;
+console.log('volumen2', volumen2+' cm3');
+const area2=base*altura+2*base*ancho;
+console.log('area2',area2+' cm2')
