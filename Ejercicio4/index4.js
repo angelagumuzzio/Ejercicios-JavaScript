5217740e8e432670b38d656dd8657b689c228e4f
angelagumuzzio <angela.gumuzzio@gmail.com>
2021-01-24 22:43:34 +0100

    Ejercicio14

--- Ejercicio4/index4.js
+++ Ejercicio4/index4.js
@@ -1,6 +1,6 @@
 var numero1=5;
 var numero2=8;
-if(numero1<numero2){console.log("el numero1 no es mayor que numero2")}
-if(numero2>0){console.log("el numero2 es positivo")}
-if(numero1<0 || numero1!=0){console.log("el numero1 es negativo o distinto de 0")}
-if(++numero1<numero2){console.log("incrementar en 1 unidad el numero1 no lo hace mayor o igual que numero 2")}
+if(numero1<numero2){alert("el numero1 no es mayor que numero2")}
+if(numero2>0){alert("el numero2 es positivo")}
+if(numero1<0 || numero1!=0){alert("el numero1 es negativo o distinto de 0")}
+if(++numero1<numero2){alert("incrementar en 1 unidad el numero1 no lo hace mayor o igual que numero 2")}
