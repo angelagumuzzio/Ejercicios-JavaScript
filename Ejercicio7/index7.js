8715ace721e19fe9ca6731c2aae801343a961968
angelagumuzzio <angela.gumuzzio@gmail.com>
2021-01-27 20:09:05 +0100

    Ejercicio7

---
+++ Ejercicio7/index7.js
@@ -0,0 +1,10 @@
+
+function parimpar(numero){
+    if (numero%2==0){
+        console.log('El número es par')
+    } 
+    else{console.log('el número es impar')} 
+}
+let num = prompt("escribe un número")
+console.log(num)
+parimpar(num)
