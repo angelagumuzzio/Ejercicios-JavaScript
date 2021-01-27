89acc16d87d12fd279a511124f0b9a693bf9e2fc
angelagumuzzio <angela.gumuzzio@gmail.com>
2021-01-27 21:10:01 +0100

    Ejercicio9

---
+++ Ejercicio9/Ejercicio9.js
@@ -0,0 +1,6 @@
+const topics = ['JavaScript', 'Variables', 'funciones', 'condicionales', 'bucles'];
+console.log('topics',topics)
+const MAY=topics.map(topic=>topic.toLocaleUpperCase())
+console.log(MAY)
+MAY.reverse();
+console.log(MAY)
