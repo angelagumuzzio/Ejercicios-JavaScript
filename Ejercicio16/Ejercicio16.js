eed9fb35a9badf4f71995cec981aabe8f7a54159
angelagumuzzio <angela.gumuzzio@gmail.com>
2021-01-28 21:21:48 +0100

    Ejercicio16

---
+++ Ejercicio16/Ejercicio16.js
@@ -0,0 +1,11 @@
+class cuadrado { 
+    constructor(lado){
+        this.perimetro=lado*4;
+        this.area=lado*lado;
+        console.log(`Este cuadrado tiene ${lado} cm de lado, su perímetro es ${this.perimetro} cm y su área ${this.area} cm3`);
+    }
+};
+
+const pequeño = new cuadrado(2);
+const mediano = new cuadrado(5);
+const grande = new cuadrado(10);
